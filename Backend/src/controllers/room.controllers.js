238f182303d3e2cb7370aab62038837664f6002b
MD SHAKIB <mdshakib@M-1.local>
2025-11-25 22:46:56 -0800

    room

--- Backend/src/controllers/room.controllers.js
+++ Backend/src/controllers/room.controllers.js
@@ -16,7 +16,7 @@ import {Apierror} from "../utils/Apierror.util.js"
 import {ApiResponse} from "../utils/ApiResponse.util.js"
 import {asyncHandler} from "../utils/asyhandler.util.js"
 import {User} from "../models/user.models.js"
-import { Room } from "../models/room.model.js";
+import {Room} from "../models/room.model.js";
 
 // Create a room – Add a new room to the system.
 
@@ -113,9 +113,144 @@ const UpdateRoom = asyncHandler(async(req, res) =>{
     
     return res.status(200).json(new ApiResponse(200, updateDetails, "ROOM DETAILS IS UPDATE SUCCESSFULLY."))
 })
+
+// Delete a room – Remove a room when decommissioned.
+const deleteRoom = asyncHandler(async(req, res) =>{
+    const {roomId} = req.params;
+    
+    if (!roomId) {
+        throw new Apierror(400, "ROOM IS IS REQUIRED FIELDS .")
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        throw new Apierror(400, "USER DOSE NOT EXIST.")
+    }
+
+    const checkRoom = await Room.findById(roomId);
+    if (!checkRoom) {
+        throw new Apierror(400, "ROOM IS NOT EXIST.")
+    }
+
+    const deleterooms = await Room.findByIdAndDelete(roomId);
+
+    if (!deleterooms) {
+        throw new Apierror(400, "ROOM IS NOT DELETE.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, deleterooms, "ROOM IS DELETE SUCCESSFULLY."))
+
+
+})
+
+// Fetch a room by ID – Get room info for a specific room.
+
+const fetchedRoom = asyncHandler(async(req, res) =>{
+    const {roomId} = req.params
+
+    if (!roomId) {
+        throw new Apierror(400, "ROOM ID IS REQUIRED FIELDS.")
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new Apierror(400, "USER DOSE NOT EXIST.")
+    }
+
+    const fetched = await Room.findById(roomId);
+
+    if (!fetched) {
+        throw new Apierror(400, "ROOM IS NOT FETCHED.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, fetched, "ROOM IS FETCHED SUCCESSFULLY."))
+
+})
+
+// Check bed availability – Check how many beds are free.
+const checkbedavailable = asyncHandler(async(req, res) =>{
+    const {roomId} = req.params
+
+    if (!roomId) {
+        throw new Apierror(400, "ROOM  ID IS REQUIRED FIELDS.")
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new Apierror(400, "USER DOSE NOT EXIST.")
+    }
+
+    const check = await Room.findById(roomId);
+
+    if (!check) {
+        throw new Apierror(400, "ROOM IS NOT EXIST")
+    }
+
+    if (check.bed_available === 0) {
+        return res.status(200).json(new ApiResponse(200, [], "BED IS NOT AVAILABLE OF THIS ROOM"))
+    }
+
+    return res.status(200).json(new ApiResponse(200, check, "BED IS AVAILABLES."))
+})
+
+// Fetch all rooms – List all rooms (useful for dashboards).
+const fetchedAllRooms  = asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new Apierror(400, "USER DOSE NO EXIST.")
+    }
+
+    const fetchedallrooms = await Room.find();
+
+    if (fetchedallrooms.length === 0) {
+        throw new Apierror(400, "ROOM IS NOT EXIST.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, fetchedallrooms, "ALL ROOM IS FETCHED SUCCESSFULLY."))
+})
+
+// Fetch rooms by type – Filter rooms by General, Private, ICU.
+const fetchedbyfilter = asyncHandler(async(req, res) =>{
+    const {roombytype} = req.params
+
+    if (!roombytype) {
+        throw new Apierror(400, "ROOM TYPES IS REQUIRED FIELDS.")
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new Apierror(400, "USER DOSE NOT EXIST.")
+    }
+
+    const filter = await Room.find({
+        type : roombytype
+    })
+
+    if (filter.length === 0) {
+        throw new Apierror(400, "THIS PARTICULAR ROOM IS NOT EXIST.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, filter, "ROOM IS FETCHED SUCCESSFULLY"))
+})
+
+
+
+
+
 export {
     createRoom,
-    UpdateRoom
+    UpdateRoom,
+    deleteRoom,
+    fetchedRoom,
+    checkbedavailable,
+    fetchedAllRooms,
+    fetchedbyfilter,
+    
 }
 
 
--- Backend/src/routes/room.routes.js
+++ Backend/src/routes/room.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import {createRoom, UpdateRoom} from "../controllers/room.controllers.js"
+import {createRoom, UpdateRoom, deleteRoom, fetchedRoom, checkbedavailable, fetchedAllRooms, fetchedbyfilter} from "../controllers/room.controllers.js"
 
 const router = Router();
 router.use(verifyJWT);
@@ -9,5 +9,13 @@ router.route("/create").post(createRoom)
 
 router.route("/updateDetails/:roomId").patch(UpdateRoom)
 
+router.route("deleteroom/:roomId").delete(deleteRoom)
 
+router.route("/fetchedroom/:roomId").get(fetchedRoom)
+
+router.route("/checkava/:roomId").get(checkbedavailable)
+
+router.route("/fetchedall").get(fetchedAllRooms)
+
+router.route("/typesroom/:roombytype").get(fetchedbyfilter)
 export default router
